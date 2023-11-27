214468d2e2e73de2ca3aaf8d2464aaf1d6dd4776
armaansait-uslap <armaansait.as@gmail.com>
2023-11-27 18:42:48 -0500

    Updated Weather

--- pma/admin.py
+++ pma/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Museum, Usage
+from .models import Museum, Usage, Weather
 
 admin.site.register(Museum)
 admin.site.register(Usage)
+admin.site.register(Weather)
