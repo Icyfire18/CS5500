329aba0f4e3f9e3a55015c7acc6a4a6b22f12bf4
armaansait-uslap <armaansait.as@gmail.com>
2023-11-27 20:16:50 -0500

    Initial Unit Test

--- pma/tests.py
+++ pma/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+import pytest
+from .models import Museum, Usage, Weather
 
-# Create your tests here.
+# # Create your tests here.
+# @pytest.mark.django_db
+# def test_create_model():
+#     Weather.objects.create(date ='11/02/2024', temperature = 24.5, location = "Portland", elevation  = 10.1, latitude  = 20, longitude = 15.5)
+#     assert Weather.objects.count() == 1
+
+class WeatherModelTestCase(TestCase):
+    def test_create_weather_model(self):
+        Weather.objects.create(
+            date='11/02/2024',
+            temperature=24.5,
+            location="Portland",
+            elevation=10.1,
+            latitude=20,
+            longitude=15.5
+        )
+        self.assertEqual(Weather.objects.count(), 1)
+
+    def test_model_str_representation(self):
+        weather_instance = Weather.objects.create(
+            date='11/02/2024',
+            temperature=24.5,
+            location="Portland",
+            elevation=10.1,
+            latitude=20,
+            longitude=15.5
+        )
+        self.assertEqual(weather_instance.location, 'Portland')
+
+    def test_model_update(self):
+        weather_instance = Weather.objects.create(
+            date='11/02/2024',
+            temperature=24.5,
+            location="Portland",
+            elevation=10.1,
+            latitude=20,
+            longitude=15.5
+        )
+        weather_instance.temperature = 25.0
+        weather_instance.save()
+        updated_instance = Weather.objects.get(pk=weather_instance.pk)
+        self.assertEqual(updated_instance.temperature, 25.0)
+
+    def test_model_deletion(self):
+        weather_instance = Weather.objects.create(
+            date='11/02/2024',
+            temperature=24.5,
+            location="Portland",
+            elevation=10.1,
+            latitude=20,
+            longitude=15.5
+        )
+        weather_instance.delete()
+        self.assertEqual(Weather.objects.count(), 0)
