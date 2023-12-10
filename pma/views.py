22abb521b464ded086f30d561e88b728513d70e7
armaansait-uslap <armaansait.as@gmail.com>
2023-12-10 15:26:06 -0500

    Corrected Errors

--- pma/views.py
+++ pma/views.py
@@ -49,16 +49,20 @@ def register(request):
         password = request.POST['password']
         password2 = request.POST['password2']
 
-        if password == password2:
-            if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists():
-                messages.info(request, 'Email or Username already exists')
-                return redirect('register')
+        if username != '' or email != '' or password != '':
+            if password == password2:
+                if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists():
+                    messages.info(request, 'Email or Username already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+                    user.save()
+                    return redirect('login')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
-                user.save()
-                return redirect('login')
+                messages.info(request, 'Password does not match')
+                return redirect('register')
         else:
-            messages.info(request, 'Password does not match')
+            messages.info(request, 'Username/Email/password Cant be Null')
             return redirect('register')
     else:
         return render(request, 'register.html')
